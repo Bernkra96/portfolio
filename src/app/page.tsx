d6970348b12c3cffbfa68fa9313b4db3272ec7d6
Mona Lisa Octocat <bernkra@gmail.com>
2024-09-23 10:52:09 +0200

    Add p text box an h text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,24 @@ export default function Home() {
           Bernhard portfolio
         </h1>
 
+        <h2 className="text-5xl text-center sm:text-left font-[family-name:var(--font-geist-mono)">
+          {' '}
+          Game And Web Dev
+        </h2>
+
+        <p className="text-2xl text-center sm:text-left font-[family-name:var(--font-geist-mono)">
+          Game And Web Dev Game And Web Dev Game And Web Dev Game And Web Dev
+        </p>
+
+        <h2 className="text-4xl text-center sm:text-left font-[family-name:var(--font-geist-mono)">
+          {' '}
+          Skills
+        </h2>
+
+        <p className="text-2xl text-center sm:text-left font-[family-name:var(--font-geist-mono)">
+          Game And Web Dev Game And Web Dev Game And Web Dev Game And Web Dev
+        </p>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border bg-zinc-700 border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
