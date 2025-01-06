0ca1246846e0286e6311e9182759c0deac03e2bd
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-01-06 20:19:15 +0100

    Macke Nav Bar and banner Stiky

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center sm:p-5  bg-green-300  dark:bg-green-600   ">
+    <div className="sticky top-0 z-50 row-start-3 flex gap-6 flex-wrap items-center justify-center sm:p-5  bg-green-300  dark:bg-green-600">
       <a
         className="rounded-full border bg-gray-300  dark:bg-gray-900 border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         href="#projects"
